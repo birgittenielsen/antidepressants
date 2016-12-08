0386538780db49c26237caa5c3dd879a43d62be2
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-08 10:27:00 +0100

    ENH: Give t test for all parameters and suggest how to run correlation
    T tests run with lapply but the cor() is not in use jet bacause of
    difficulties with mapply()

---
+++ source/ecs.fall.R
@@ -0,0 +1,32 @@
+ecs.fall <- read.csv("./data/processed/re.ecs.fall.csv", header=TRUE)
+
+cols = ecs.fall[4:25]
+colnames = names(ecs.fall[4:25])
+tests = lapply(data, function(x) {
+  t.test(x ~ Gruppe, data=ecs.fall, var.equal=TRUE)
+})
+
+for(df in ) {
+  print(col)
+  print(colname)
+}
+
+cor(data.matrix(S), method="pearson")
+
+# alpha = 0.05
+# for(name in names(ecs.fall)) {
+#   ttest = t.test(ecs.fall[[name]] ~ ecs.fall$Gruppe, var.equal=TRUE)
+#   if(ttest["p.value"] < alpha) {
+#     print(name)
+#     print(ttest)
+#   }
+# if(ttest["p.value"] < alpha) {
+#   print(ttest)
+#   significant <- cbind(significant, x)
+# }S = data.frame(matrix(NA,14,0))
+# A = mapply(function(col, colname, test) {
+#   if(test["p.value"] < 0.05) {
+#     return(col)
+#   }
+# }, cols, colnames, tests, SIMPLIFY=TRUE)
+# }
