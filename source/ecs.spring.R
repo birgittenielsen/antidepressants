b07ae9b61b2ef8ec27544cf505e374b5670d874e
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 09:15:34 +0100

    BUG: Change significant level to 0.04

--- source/ecs.spring.R
+++ source/ecs.spring.R
@@ -4,7 +4,7 @@ cols = ecs.spring[4:25]
 colnames = names(ecs.spring[4:25])
 tests = lapply(cols, function(x) {
   test = t.test(x ~ Gruppe, data=ecs.spring, var.equal=TRUE)
-  if(test["p.value"] < 0.05) {
+  if(test["p.value"] < 0.04) {
     return(test)
   } else {
     return("Not statistically significant")
