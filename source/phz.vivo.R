79ef57aecb1da73bd3ae10409f26e3f187d3e305
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 13:06:38 +0100

    ENH: Subset dataframes with sepcific groups + t test

--- source/phz.vivo.R
+++ source/phz.vivo.R
@@ -19,3 +19,10 @@ for(name in names(tests)) {
   print(name)
   print(tests[[name]])
 }
+
+#Significant for H2O2 ROX in Group 2-0 and 2-1
+phz.vivo20 <- subset(phz.vivo[which(phz.vivo$Gruppe == 2 | phz.vivo$Gruppe == 0),])
+phz.vivo21 <- subset(phz.vivo[which(phz.vivo$Gruppe == 2 | phz.vivo$Gruppe == 1),])
+
+t.test(H202.ROX ~ Gruppe, data=phz.vivo20, var.equal=TRUE)
+t.test(H202.ROX ~ Gruppe, data=phz.vivo21, var.equal=TRUE)
