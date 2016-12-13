67cbec51f9aa97a6164ff4377722633a819e6945
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 14:24:02 +0100

    BUG: Deletes var.equal=TRUE

--- source/phz.vivo.oxo.R
+++ source/phz.vivo.oxo.R
@@ -5,7 +5,7 @@ phz.vivo.oxo <- read.csv("./data/processed/re.phz.vivo.oxo.csv", header=TRUE)
 cols = phz.vivo.oxo[4:25]
 colnames = names(phz.vivo.oxo[4:25])
 tests = lapply(cols, function(x) {
-  test = t.test(x ~ Gruppe, data=phz.vivo.oxo, var.equal=TRUE)
+  test = t.test(x ~ Gruppe, data=phz.vivo.oxo)
   if(test["p.value"] < 0.05) {
     return(test)
   } else {
