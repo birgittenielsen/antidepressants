4ff04c214be952084857d926be05faf3d5349ec2
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 09:21:27 +0100

    ENH: Run ANOVA (+TukeyHSD) instead of t test

--- source/phz.vivo.R
+++ source/phz.vivo.R
@@ -1,7 +1,21 @@
 phz.vivo <- read.csv("./data/processed/re.phz.vivo.csv", header=TRUE)
 
-cols = phz.vivo[4:25]
-colnames = names(phz.vivo[4:25])
-tests = lapply(data, function(x) {
-  t.test(x ~ Gruppe, data=phz.vivo, var.equal=TRUE)
+cols <- phz.vivo[4:24]
+colnames <- names(phz.vivo[4:24])
+
+tests <- lapply(cols, function(x) {
+  lm_result <- lm(x ~ factor(Gruppe), data=phz.vivo)
+  anova_result <- anova(lm_result)
+  aov_result <- aov(lm_result)
+  TukeyHSD_result <- TukeyHSD(aov_result)
+  if(anova_result[1,5] < 0.05) {
+    return(list(TRUE, TukeyHSD_result)) 
+  } else {
+    return(list(FALSE))
+  }
 })
+
+for(name in names(tests)) {
+  print(name)
+  print(tests[[name]])
+}
