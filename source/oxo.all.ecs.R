db7050b7aae4a13c6c0f9c1ec218c4c1ee8a9307
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 13:04:48 +0100

    BUG: Puts NAs in oxo.all instead of oxo.all.ecs
    The ruined tissues should be eliminated before subsetting to ecs

--- source/oxo.all.ecs.R
+++ source/oxo.all.ecs.R
@@ -1,9 +1,10 @@
 oxo.all <- read.csv("./data/raw/oxo.all.csv", header=TRUE, sep=";", dec=",")
 
-oxo.all.ecs <- oxo.all[17:44,]
 
 for(id in c(9,4,13,19,12,16,14,11)) {
-  oxo.all.ecs <- oxo.all.ecs[oxo.all.ecs$ID != id,]
+  oxo.all <- oxo.all[oxo.all$ID != id,]
 }
+
+oxo.all.ecs <- oxo.all[17:44,]
 
 t.test(Ratio ~ Gruppe, data=oxo.all.ecs, var.equal=TRUE)
