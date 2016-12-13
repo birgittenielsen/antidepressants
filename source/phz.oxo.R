c09be519740af514db560870ac13d8a87870cb07
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 09:16:34 +0100

    ENH: Read oxo file and run t test

---
+++ source/phz.oxo.R
@@ -0,0 +1,6 @@
+oxo.all <- read.csv("./data/raw/oxo.all.csv", header=TRUE, sep=";", dec=",")
+
+oxo.phz <- oxo.all[1:16, ]
+oxo.phz <- oxo.phz[order(oxo.phz$ID),]
+
+t.test(Ratio ~ Gruppe, data=oxo.phz, var.equal=TRUE)
