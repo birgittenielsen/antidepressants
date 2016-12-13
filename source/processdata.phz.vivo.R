8aa4d2a4f12371aaaefc73e6256fff99eea980ed
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 16:34:42 +0100

    BUG: Changes new ID, tab mistake and subsetting with less code

--- source/processdata.phz.vivo.R
+++ source/processdata.phz.vivo.R
@@ -12,9 +12,9 @@ re.phz.vivo <- re.phz.vivo[order(re.phz.vivo$ID),]
 re.phz.vivo <- subset(re.phz.vivo, select=-c(FCR.ETS, FPM.ROX, FCR.ROX))
 
 #Subsetting weight dataframe 
-weight.phz.vivo <- subset(weight, ID > 20 & ID < 39)
+weight.phz.vivo <- subset(weight, ID > 20 & ID < 281)
 
-stopifnot(all(re.phz.vivo$ID == weight.ecs.spring.o$ID))
+stopifnot(all(re.phz.vivo$ID == weight.phz.vivo$ID))
 
 #OXO processing 
 #Choosing oxo data 
@@ -22,7 +22,7 @@ oxo.phz <- subset(oxo.all, ID > 20 & ID < 39)
 oxo.phz <- oxo.phz[order(oxo.phz$ID),]
 
 #PHZ dataframe with all data in group 0 and 1 to obtain oxo data
-re.phz.vivo.oxo <- re.phz.vivo[which(re.phz.vivo$Gruppe == 0 | re.phz.vivo$Gruppe == 1),]
+re.phz.vivo.oxo <- subset(phz.vivo, Gruppe == 2 | Gruppe == 0)
 #Oxo data included in reshape PHZ vivo data
 re.phz.vivo.oxo$oxo <- oxo.phz[which(oxo.phz$ID != 23),]$Ratio
 
