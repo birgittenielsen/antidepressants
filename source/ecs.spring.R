07d2b56c2026bfc51694e2d691db33aea45c4ad6
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-13 14:23:25 +0100

    ENH: Integrete new file with weight data, change the alpha to 0.05

--- source/ecs.spring.R
+++ source/ecs.spring.R
@@ -1,12 +1,19 @@
 ecs.spring <- read.csv("./data/processed/re.ecs.spring.csv", header=TRUE)
 
-cols = ecs.spring[4:25]
-colnames = names(ecs.spring[4:25])
+cols = ecs.spring[4:26]
+colnames = names(ecs.spring[4:26])
 tests = lapply(cols, function(x) {
-  test = t.test(x ~ Gruppe, data=ecs.spring, var.equal=TRUE)
-  if(test["p.value"] < 0.04) {
+  test = t.test(x ~ Gruppe, data=ecs.spring)
+  if(test["p.value"] < 0.05) {
     return(test)
   } else {
     return("Not statistically significant")
   }
 })
+
+
+for(name in names(tests)) {
+  print(name)
+  print(tests[[name]])
+}
+
