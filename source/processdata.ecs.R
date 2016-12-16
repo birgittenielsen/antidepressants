cfcecdc7495b990171396d3be87bd8606e808a0f
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-16 13:43:57 +0100

    ENH: integrates all observations in one dataframe
    By integrating all observations from different files (oroboros, oxo and
    weight) can I avoid having multiple script for each file when running
    tests and I will avoid "deleting" specifik observations because it is
    not availabel in oroboros or oxo

--- source/processdata.ecs.R
+++ source/processdata.ecs.R
@@ -2,9 +2,9 @@
 ecs.spring <- read.csv("./data/raw/ecs.spring.csv", header=TRUE, sep=";", dec=",")
 ecs.fall <- read.csv("./data/raw/ecs.fall.csv", header=TRUE, sep=";", dec=",")
 oxo.all <- read.csv("./data/raw/oxo.all.csv", header=TRUE, sep=";", dec=",")
-weight <- read.csv("./data/raw/weight.csv", header=TRUE, sep=";", dec=",")
+weight <- read.csv("./data/raw/weight.copy.csv", header=TRUE, sep=";", dec=",")
 
-names <- names(oxo.all)
+weight <- weight[order(weight$ID),]
 
 # Oxo tissue ruined - should not be included
 for( id in c(9,4,13,19,12,16,14,11) ) {
@@ -26,35 +26,30 @@ re.ecs.spring <- subset(re.ecs.spring, select=-c(FCR.ETS, FPM.ROX, FCR.ROX))
 
 # Subset oxo data for including the data into reshaped data frame
 oxo.ecs.spring <- subset(oxo.all, ID > 1000)
-oxo.ecs.spring.o <- subset(oxo.ecs.spring, ID != 4001)
-
 oxo.ecs.fall <- subset(oxo.all, ID < 21)
-oxo.ecs.fall.o <- subset(oxo.ecs.fall, ID != 13 & ID!= 18)
 
 #Check if number of observations are the shape before including of oxo data
-stopifnot(all(re.ecs.spring$ID == oxo.ecs.spring.o$ID))
-stopifnot(all(re.ecs.fall$ID == oxo.ecs.fall.o$ID))
+stopifnot(all(re.ecs.spring$ID == oxo.ecs.spring$ID))
+stopifnot(all(re.ecs.fall$ID == oxo.ecs.fall$ID))
 
 #Oxo data included in reshape data 
-re.ecs.spring$oxo <- oxo.ecs.spring.o$Ratio
-re.ecs.fall$oxo <- oxo.ecs.fall.o$Ratio
+re.ecs.spring$oxo <- oxo.ecs.spring$Ratio
+re.ecs.fall$oxo <- oxo.ecs.fall$Ratio
 
 #Subsets weight dataframes
 weight.ecs.spring <- subset(weight, ID > 1000)
-weight.ecs.spring <- weight.ecs.spring[order(weight.ecs.spring$ID),]
 
 weight.ecs.fall <- subset(weight, ID < 21)
-weight.ecs.fall <- weight.ecs.fall[order(weight.ecs.fall$ID),]
 
 weight.ecs.spring.o <- subset(weight.ecs.spring, ID != 2002 & ID != 4001)
 weight.ecs.fall.o <- subset(weight.ecs.fall, ID != 13 & ID != 18)
 
-stopifnot(all(re.ecs.spring$ID == weight.ecs.spring.o$ID))
-stopifnot(all(re.ecs.fall$ID == weight.ecs.fall.o$ID))
+stopifnot(all(re.ecs.spring$ID == weight.ecs.spring$ID))
+stopifnot(all(re.ecs.fall$ID == weight.ecs.fall$ID))
 
 #Include weight data in reshaped dataframes
-re.ecs.spring$weight <- weight.ecs.spring.o$Weight
-re.ecs.fall$weight <- weight.ecs.fall.o$Weight
+re.ecs.spring$weight <- weight.ecs.spring$Weight
+re.ecs.fall$weight <- weight.ecs.fall$Weight
 
 # Save as csv files in data/processed
 write.csv(re.ecs.spring, file="./data/processed/re.ecs.spring.csv")
