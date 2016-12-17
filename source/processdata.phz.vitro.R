85faa2253a3e466ec027b05ab4a8edb44ffd402c
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-17 11:15:53 +0100

    BUG: Delete weight data - no purpose in in vitro

--- source/processdata.phz.vitro.R
+++ source/processdata.phz.vitro.R
@@ -11,13 +11,6 @@ re.phz.vitro <- re.phz.vitro[order(re.phz.vitro$ID),]
 # Exclude constant columns[]
 re.phz.vitro <- subset(re.phz.vitro, select=-c(FCR.ETS, FPM.ROX, FCR.ROX))
 
-#Subsetting weight dataframe 
-weight.phz.vitro <- subset(weight, ID > 38 & ID < 47)
-weight.phz.vitro <- weight.phz.vitro[order(weight.phz.vitro),]
-weight.phz.vitro <- weight.phz.vitro[1:8,]
-
-stopifnot(all(re.ecs.fall$ID == weight.ecs.fall.o$ID))
-
 #Saving as csv files in data/processed
 write.csv(re.phz.vitro, file="./data/processed/re.phz.vitro.csv")
 
