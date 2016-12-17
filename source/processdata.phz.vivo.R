568c5898788e3910e4f342c331ad0c82c0c41fd8
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-17 09:55:31 +0100

    ENH: Include weight into dataframe with oxo and oroboros only with group 0 and 1

--- source/processdata.phz.vivo.R
+++ source/processdata.phz.vivo.R
@@ -11,18 +11,24 @@ re.phz.vivo <- re.phz.vivo[order(re.phz.vivo$ID),]
 #Excluding constant columns[]
 re.phz.vivo <- subset(re.phz.vivo, select=-c(FCR.ETS, FPM.ROX, FCR.ROX))
 
-#Subsetting weight dataframe 
-weight.phz.vivo <- subset(weight, ID > 20 & ID < 281)
 
-stopifnot(all(re.phz.vivo$ID == weight.phz.vivo$ID))
+#PHZ dataframe with all data in group 0 and 1 to obtain oxo and weight data
+re.phz.vivo.ow <- subset(re.phz.vivo, Gruppe == 1 | Gruppe == 0)
+
+#Subsetting weight dataframe 
+weight.phz.vivo <- subset(weight, ID > 20 & ID < 39)
+weight.phz.vivo <- weight.phz.vivo[order(re.phz.vivo$ID),]
+
+stopifnot(all(re.phz.vivo.ow$ID == weight.phz.vivo$ID))
+
+#Including weight data into new dataframe 
+re.phz.vivo.ow$weight <- weight.phz.vivo
 
 #OXO processing 
 #Choosing oxo data 
 oxo.phz <- subset(oxo.all, ID > 20 & ID < 39)
 oxo.phz <- oxo.phz[order(oxo.phz$ID),]
 
-#PHZ dataframe with all data in group 0 and 1 to obtain oxo data
-re.phz.vivo.oxo <- subset(phz.vivo, Gruppe == 2 | Gruppe == 0)
 #Oxo data included in reshape PHZ vivo data
 re.phz.vivo.oxo$oxo <- oxo.phz[which(oxo.phz$ID != 23),]$Ratio
 
