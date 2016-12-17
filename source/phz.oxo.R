dfbf776838196ed389ccfe7b231f4cd1595ef84a
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-17 11:13:55 +0100

    BUG: Delete script with oxo data - now integrated in phz.vivo.ow

--- source/phz.oxo.R
+++
@@ -1,6 +0,0 @@
-oxo.all <- read.csv("./data/raw/oxo.all.csv", header=TRUE, sep=";", dec=",")
-
-oxo.phz <- oxo.all[1:16, ]
-oxo.phz <- oxo.phz[order(oxo.phz$ID),]
-
-t.test(Ratio ~ Gruppe, data=oxo.phz,)
