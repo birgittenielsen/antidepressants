584d0d708fa4b3ac5e29baa47d7dfd0e57890ec5
birgittenielsen <birgitte_lopi@hotmail.com>
2016-12-20 12:36:45 +0100

    ENH: Create subset data frame with only correlations of interest

--- source/ecs.spring.cor.R
+++ source/ecs.spring.cor.R
@@ -25,7 +25,9 @@ test.c <- rcorr(ecs.spring.cm, type="pearson")
 
 results.cm <- results(test.c$r, test.c$P)
 
-control <- results.cm[which(results.cm$cor > 0.8 | results.cm$cor < -0.8 & results.cm$p < 0.05),]
+control <- subset(results.cm, cor > 0.8 | cor < -0.8 & p < 0.05)
+  
+control.in <- subset(control, column == "H202.ROX" | column == "oxo" | column == "weight")
 
 #Correlation for treated group
 test.t <- rcorr(ecs.spring.tm, type="pearson")
@@ -34,3 +36,4 @@ results.tm <- results(test.t$r, test.t$P)
 
 treated <- results.tm[which(results.tm$cor > 0.8 | results.tm$cor < -0.8 & results.tm$p < 0.05),]
 
+treated.in <- subset(treated, column == "H202.ROX" | column == "oxo" | column == "weight")
