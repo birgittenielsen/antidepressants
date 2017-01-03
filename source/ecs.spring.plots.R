46652b593f118f6794857e8eb5a1d9105d688ec7
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-03 09:50:45 +0100

    ENH: Create plots for ECS Spring data

---
+++ source/ecs.spring.plots.R
@@ -0,0 +1,33 @@
+ecs.spring <- read.csv("./data/processed/re.ecs.spring.csv", header=TRUE)
+
+plot.oxo <- boxplot(oxo ~ Gruppe, data=ecs.spring, 
+        main="ECS Spring - 8oxoGuo Ratio", 
+        ylab="8oxoGuo Pr. 10E6 Guo", 
+        names=c("Control", "ECS"), 
+        col=c(8,11))
+
+plot.weight <- boxplot(weight ~ Gruppe, data=ecs.spring,
+                       main="ECS Spring - Weight",
+                       ylab="Gram",
+                       names=c("Control", "ECS"),
+                       las=1, 
+                       col=c(8,11))
+              
+
+ecs.spring. <- read.csv("./data/raw/ecs.spring.csv", header=TRUE, sep=";", dec=",")
+
+plot.h202 <- boxplot(H202 ~ Gruppe*State, data=ecs.spring., 
+        col=c(8,11), 
+        main="ECS Spring - H2O2 Flux", 
+        ylab="pmol/ml*s", 
+        names=c("Complex I", "Complex I", "Complex I+II", "Complex I+II", "Complex II", "Complex II", "Complex III", "Complex III", "ETS", "ETS", "LEAK", "LEAK", "ROUTINE", "ROUTINE", "ROX", "ROX"),
+        las=2,)
+        legend("topleft", c("Control", "ECS"), col=c(8,11), pch=15)
+        
+plot.o2 <- boxplot(FPM ~ Gruppe*State, data=ecs.spring., 
+                   col=c(8,11), 
+                   main="ECS Spring - O2 Flux", 
+                   ylab="pmol/ml*s", 
+                   names=c("Complex I", "Complex I", "Complex I+II", "Complex I+II", "Complex II", "Complex II", "Complex III", "Complex III", "ETS", "ETS", "LEAK", "LEAK", "ROUTINE", "ROUTINE", "ROX", "ROX"),
+                   par(las=2))
+          legend("topleft", c("Control", "ECS"), col=c(8,11), pch=15)
