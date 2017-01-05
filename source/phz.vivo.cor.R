577a1358f29494278bb8a1fb3def2f9537b9823d
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-05 10:54:21 +0100

    BUG: Add correct library + more specific results

--- source/phz.vivo.cor.R
+++ source/phz.vivo.cor.R
@@ -11,6 +11,8 @@ phz.vivo.cm <- as.matrix(phz.vivo.c)
 phz.vivo.t1m <- as.matrix(phz.vivo.t1)
 phz.vivo.t2m <- as.matrix(phz.vivo.t2)
 
+library(Hmisc)
+
 #rcorr returns r: the correlation matrix, n, p
 results <- function(cormat, pmat) {
   ut <- upper.tri(cormat)
@@ -29,6 +31,8 @@ results.cm <- results(test.c$r, test.c$P)
 
 control <- results.cm[which(results.cm$cor > 0.8 | results.cm$cor < -0.8 & results.cm$p < 0.05),]
 
+control.in <- subset(control, column=="H202.ROX" | column=="weight" | column=="oxo")
+
 #Correlation for treated group 1
 test.t1 <- rcorr(phz.vivo.t1m, type="pearson")
 
@@ -36,7 +40,7 @@ results.t1m <- results(test.t1$r, test.t1$P)
 
 invivo <- results.t1m[which(results.t1m$cor > 0.8 | results.t1m$cor < -0.8 & results.t1m$p < 0.05),]
 
-
+invivo.in <- subset(invivo, column=="H202.ROX" | column=="weight" | column=="oxo")
 
 #Correlation for treated group 2
 test.t2 <- rcorr(phz.vivo.t2m, type="pearson")
@@ -45,4 +49,4 @@ results.t2m <- results(test.t2$r, test.t2$P)
 
 invivoinvitro <- results.t2m[which(results.t2m$cor > 0.8 | results.t2m$cor < -0.8 & results.t2m$p < 0.05),]
 
-
+invivoinvitro.in <- subset(invivoinvitro, column=="H202.ROX" | column=="weight" | column=="oxo")
