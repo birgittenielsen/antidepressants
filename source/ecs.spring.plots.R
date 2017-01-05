9c0afae5d834d320cd4ff7929e6a0f06db8123ac
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-05 11:22:43 +0100

    ENH: Create new boxplot without outliner

--- source/ecs.spring.plots.R
+++ source/ecs.spring.plots.R
@@ -4,7 +4,25 @@ plot.oxo <- boxplot(oxo ~ Gruppe, data=ecs.spring,
         main="ECS Spring - 8oxoGuo Ratio", 
         ylab="8oxoGuo Pr. 10E6 Guo", 
         names=c("Control", "ECS"), 
-        col=c(8,11))
+        col=c(8,11), 
+        las=1)
+
+#Delete outliner
+ecs.spring.red <- subset(ecs.spring, oxo != 66.7)
+
+plot.oxo.red <- boxplot(oxo ~ Gruppe, data=ecs.spring.red, 
+                    main="ECS Spring - 8oxoGuo Ratio", 
+                    ylab="8oxoGuo Pr. 10E6 Guo", 
+                    names=c("Control", "ECS"), 
+                    col=c(8,11), 
+                    las=1, 
+                    ylim=c(0,15))
+
+a <- mean(ecs.spring.red$oxo[which(ecs.spring.red$Gruppe==0)], na.rm=TRUE)
+b <- mean(ecs.spring.red$oxo[which(ecs.spring.red$Gruppe==1)], na.rm=TRUE)
+text(1,8, sprintf("mean %.1f",round(a,1)))
+text(2,9.7, sprintf("mean %.1f",round(b,1)))
+
 
 plot.weight <- boxplot(weight ~ Gruppe, data=ecs.spring,
                        main="ECS Spring - Weight",
