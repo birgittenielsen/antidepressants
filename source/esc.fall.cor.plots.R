204a93d488c384de7a398d26df1dd13131d5bf1c
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-05 10:52:13 +0100

    BUG: Correct legend

--- source/esc.fall.cor.plots.R
+++ source/esc.fall.cor.plots.R
@@ -2,14 +2,24 @@ ecs.fall <- read.csv("./data/processed/re.ecs.fall.csv", header=TRUE)
 
 library(car)
 
-ecs.fall.0 <- subset(ecs.fall, Gruppe==0)
-ecs.fall.1 <- subset(ecs.fall, Gruppe==1)
 
 #Scatterplot med H202 og vægt i forhold til grupper
 scatterplot(H202.ROX ~ weight | Gruppe, data=ecs.fall, 
             main="Electroconvulsive Stimulation", 
-            ylab="H202 ROX Flux (pmol/ml*s)", xlab="Weight (g)",
-            ylim=c(0,1.7))
+            ylab="H202 ROX Flux (pmol/ml*s)", 
+            xlab="Weight (g)",
+            ylim=c(0,1.7),
+            legend.plot=FALSE
+)
+
+legend("bottomright", 
+       c("Control", "ECS"), 
+       pch=c(1,2), 
+       col=c(1,2), 
+       title="Group")
+
+ecs.fall.0 <- subset(ecs.fall, Gruppe==0)
+ecs.fall.1 <- subset(ecs.fall, Gruppe==1)
 
 #Matrix scatterplots med respiration             
 pairs(~FPM.CII+FPM.CI.CII+FPM.ETS,data=ecs.fall.1, 
@@ -24,5 +34,6 @@ pairs(~FPM.CII+FPM.CI.CII+FPM.ETS,data=ecs.fall,
 #Correlation med oxo og H202 ROX 
 scatterplot(H202.ROX ~ oxo | Gruppe, data=ecs.fall, 
             main="Electroconvulsive Stimulation", 
-            ylab="H202 ROX Flux (pmol/ml*s)", xlab="8oxoGuo",
+            ylab="H202 ROX Flux (pmol/ml*s)", 
+            xlab="8oxoGuo",
             ylim=c(0,1.7))
