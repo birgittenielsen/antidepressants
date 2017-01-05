0350540e8db749669ff26d634c894fb759e9261d
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-05 10:53:09 +0100

    BUG: Delete unused function, ENH: Create boxplot

--- source/oxo.ecs.all.R
+++ source/oxo.ecs.all.R
@@ -1,12 +1,28 @@
 oxo.all <- read.csv("./data/raw/oxo.all.csv", header=TRUE, sep=";", dec=",")
+oxo.all <- oxo.all[order(oxo.all$ID),]
 
-
-for(id in c(9,4,13,19,12,16,14,11)) {
-  oxo.all <- oxo.all[oxo.all$ID != id,]
-}
-
-oxo.all.ecs <- oxo.all[17:44,]
+oxo.all.ecs <- subset(oxo.all, ID < 21 | ID > 1000)
 
 t.test(Ratio ~ Gruppe, data=oxo.all.ecs)
+
+boxplot(Ratio ~ Gruppe, data=oxo.all.ecs,
+        main="ECS All - 8-oxoGuo", 
+        ylab="8oxoGuo Pr. 10E6 Guo",
+        col=c(16,11), 
+        names=c("Control", "ECS"), 
+        las=1)
+
+# ID 7002 tages ud
+oxo.all.ecs. <- subset(oxo.all.ecs, ID != 7002)
+
+t.test(Ratio ~ Gruppe, data=oxo.all.ecs.)
+
+boxplot(Ratio ~ Gruppe, data=oxo.all.ecs.,
+        main="ECS All - 8-oxoGuo", 
+        ylab="8oxoGuo Pr. 10E6 Guo",
+        col=c(16,11), 
+        names=c("Control", "ECS"), 
+        las=1)
+
 
 #Kør correlation mellem ratio og weight data
