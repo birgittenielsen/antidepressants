30f78d29cc97be47956ad00492350c3231f3a0e3
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-11 10:15:59 +0100

    BUG: Correct ylim and text position in plot

--- source/phz.vivo.plots.R
+++ source/phz.vivo.plots.R
@@ -4,19 +4,20 @@ phz.vivo <- read.csv("./data/processed/re.phz.vivo.csv", header=TRUE)
 #laves boxplot over det
 boxplot(H202.ROX ~ Gruppe, 
         data=phz.vivo, 
-        ylim=c(0,1.7), 
+        ylim=c(0,2), 
         names=c("Control", "In Vivo", "In Vivo + In Vitro"),
         col=c("olivedrab", "olivedrab1", "olivedrab3"), 
         main="Phenelzine In Vivo
         H202 Flux Non-Electron Transfer System", 
-        ylab="H202 Flux (pmol/ml*s")
+        ylab="H202 Flux (pmol/ml*s", 
+        las=1)
 
 a <- mean(phz.vivo$H202.ROX[which(phz.vivo$Gruppe==0)], na.rm=TRUE)
 b <- mean(phz.vivo$H202.ROX[which(phz.vivo$Gruppe==1)], na.rm=TRUE)
 c <- mean(phz.vivo$H202.ROX[which(phz.vivo$Gruppe==2)], na.rm=TRUE)
-text(1,1.65, sprintf("mean %.1f",round(a,1)))
-text(2,1.45, sprintf("mean %.1f",round(b,1)))
-text(3,1.2, sprintf("mean %.1f *", round(c,1)))
+text(1,1.7, sprintf("mean %.1f",round(a,1)))
+text(2,1.5, sprintf("mean %.1f",round(b,1)))
+text(3,1.25, sprintf("mean %.1f *", round(c,1)))
 
 title(sub="*:p-value<0.05, n control: 8, n in vivo: 7, n in vivo+in vitro: 6. 
       In vivo + in vitro 0.33 mM PHZ were added", adj=0.1)
