0957da01f2914f95c40387a4f338351680ac562f
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-12 11:27:30 +0100

    BUG: Changing the normalisation for involve both groups at the same time

--- source/ecs.fall.ow.R
+++ source/ecs.fall.ow.R
@@ -4,18 +4,14 @@ ecs.fall.ow <- ecs.fall[c(2:3,25:26)]
 
 ecs.fall.ow <- na.omit(ecs.fall.ow)
 
-a <- min(ecs.fall.ow$oxo[which(ecs.fall.ow$Gruppe==0)])
-b <- min(ecs.fall.ow$oxo[which(ecs.fall.ow$Gruppe==1)])
-c <- max(ecs.fall.ow$oxo[which(ecs.fall.ow$Gruppe==0)])
-d <- max(ecs.fall.ow$oxo[which(ecs.fall.ow$Gruppe==1)])
+a <- min(ecs.fall.ow$oxo)
+b <- max(ecs.fall.ow$oxo)
 
-ecs.fall.ow$norm <- NA
-
-
-ecs.fall.ow$norm[which(ecs.fall.ow$Gruppe == 1)] <- 
-  (ecs.fall.ow$oxo[which(ecs.fall.ow$Gruppe==1)]-b)/d 
-
-ecs.fall.ow$norm[which(ecs.fall.ow$Gruppe == 0)] <- 
-  (ecs.fall.ow$oxo[which(ecs.fall.ow$Gruppe==0)]-a)/c 
+ecs.fall.ow$norm <- (ecs.fall.ow$oxo-a)/b
 
 t.test(norm ~ Gruppe, data=ecs.fall.ow, var.equal=TRUE)
+
+boxplot(norm ~ Gruppe, data=ecs.fall.ow,
+        names=c("Control", "ECS"), 
+        main="ECS Fall Normalized 8oxoGuo", 
+        col=c(16,11))
