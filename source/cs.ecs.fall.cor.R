3d4f8b505b97a9165911cc2d8ce19923a149911e
birgittenielsen <birgitte_lopi@hotmail.com>
2017-01-30 08:24:01 +0100

    ENH: Add weight data

--- source/cs.ecs.fall.cor.R
+++ source/cs.ecs.fall.cor.R
@@ -1,11 +1,12 @@
 ecs.fall <- read.csv("./data/processed/re.ecs.fall.csv", header=TRUE)
 
-normalized <- ecs.fall[4:27]
+normalized <- ecs.fall[4:24]
 
 for(i in names(normalized)) {
   normalized[i] <- normalized[i]/ecs.fall$cs
 }
 
+normalized$weight <- ecs.fall$weight
 normalized$cs <-ecs.fall$cs
 normalized$ID <- ecs.fall$ID
 normalized$Gruppe <- ecs.fall$Gruppe
@@ -36,26 +37,15 @@ test.c <- rcorr(normalized.cm, type="pearson")
 
 results.cm <- results(test.c$r, test.c$P)
 
-control <- results.cm[which(results.cm$cor > 0.8 | results.cm$cor < -0.8 & results.cm$p < 0.05),]
-
-control.in <- subset(control, row=="weight" | 
-                       row=="OXCA.CII" | row=="H202.ROX" | row=="FPM.CII" | 
-                       row=="FPM.ETS" | row=="FPM.CI.CII" | column=="weight" | 
-                       column=="OXCA.CII" | column=="H202.ROX" | 
-                       column=="FPM.CII" | column=="FPM.ETS" | 
-                       column=="FPM.CI.CII" | column=="oxo")
+control.p <- subset(results.cm, cor > 0.8 & p < 0.05)
+control.n <- subset(results.cm, cor < -0.8 & p < 0.05)
 
 #Correlation for treated group
 test.t <- rcorr(normalized.tm, type="pearson")
 
 results.tm <- results(test.t$r, test.t$P)
 
-treated <- results.tm[which(results.tm$cor > 0.8 | results.tm$cor < -0.8 & results.tm$p < 0.05),]
+treated.p <- subset(results.tm, cor > 0.8 & p < 0.05)
+treated.n <- subset(results.tm, cor < 0.8 & p < 0.05)
 
-treated.in <- subset(treated, row=="weight" | 
-                       row=="OXCA.CII" | row=="H202.ROX" | row=="FPM.CII" | 
-                       row=="FPM.ETS" | row=="FPM.CI.CII" | column=="weight" | 
-                       column=="OXCA.CII" | column=="H202.ROX" | 
-                       column=="FPM.CII" | column=="FPM.ETS" | 
-                       column=="FPM.CI.CII" | column=="oxo")
 
